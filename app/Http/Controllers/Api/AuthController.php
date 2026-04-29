22a3cf5ee005cec76976f924bd67be01f90c7c8c
Martin Medrano <mmedranom@tecnocomfenalco.edu.co>
2026-04-29 15:08:00 -0500

    Añadiendo funcionalidad de logout y mejorando la respuesta del token en AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
@@ -22,13 +23,29 @@ class AuthController extends Controller
             ], 401);
         }
 
-        return response()->json([
-            'Token creado' => $token,
-        ], 201);
+        return $this->respondWithToken($token);
     }
 
     public function me()
     {
         return response()->json(Auth::guard('api')->user());
+    }
+
+    public function logout(){
+        Auth::guard('api')->logout();
+
+        return response()->json([
+            'message' => 'Sesión cerrada correctamente'
+        ]);
+    }
+
+
+    public function respondWithToken(String $token){
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => config('jwt.ttl') * 60,
+            'user'         => Auth::guard('api')->user(),
+        ]);
     }
 }
